f7f74c2ca06c751d00c81733e46c37bd974f673e
christianangelopoulos <christianangelopoulos@protonmail.com>
2025-11-22 22:39:39 -0600

    cleaned up old stuff

--- build.js
+++ build.js
@@ -1,20 +1,10 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { exec } = require('child_process');
 
 async function build() {
   // Clean public directory
   console.log('Cleaning public directory...');
   await fs.emptyDir('./public');
-
-  // Build Tailwind CSS
-  console.log('Building Tailwind CSS...');
-  await new Promise((resolve, reject) => {
-    exec('npx tailwindcss -i ./src/styles/main.css -o ./src/styles/output.css', (error) => {
-      if (error) reject(error);
-      else resolve();
-    });
-  });
 
   // Copy all files from src to public
   console.log('Copying files to public directory...');
